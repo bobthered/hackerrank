583c7ed03a72305e93806bf820a1a62c7275f481
Bob McAleavey <bobmcaleavey@gmail.com>
2020-12-14 10:50:07 -0500

    add solution for problem " 8 - Buttons Container\

---
+++ skills/10-days-of-js/8-buttons-container/js/buttonsGrid.js
@@ -0,0 +1,22 @@
+btn5.addEventListener('click', () => {
+  [
+    btn1.innerHTML,
+    btn2.innerHTML,
+    btn3.innerHTML,
+    btn4.innerHTML,
+    btn6.innerHTML,
+    btn7.innerHTML,
+    btn8.innerHTML,
+    btn9.innerHTML
+  ] =
+  [
+    btn4.innerHTML,
+    btn1.innerHTML,
+    btn2.innerHTML,
+    btn7.innerHTML,
+    btn3.innerHTML,
+    btn8.innerHTML,
+    btn9.innerHTML,
+    btn6.innerHTML
+  ]
+})
